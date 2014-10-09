e34c312e04fa9199897fcb678a1d78414a7a4b9c
robin <luo_jianguo@163.com>
2014-10-09 20:55:23 +0800

    batch

---
+++ string.s
@@ -0,0 +1,33 @@
+	.file	"string.c"
+	.text
+	.globl	StrLen
+	.type	StrLen, @function
+StrLen:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -24(%rbp)
+	movl	$0, -4(%rbp)
+	jmp	.L2
+.L3:
+	addl	$1, -4(%rbp)
+.L2:
+	movq	-24(%rbp), %rax
+	leaq	1(%rax), %rdx
+	movq	%rdx, -24(%rbp)
+	movzbl	(%rax), %eax
+	testb	%al, %al
+	jne	.L3
+	movl	-4(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	StrLen, .-StrLen
+	.ident	"GCC: (Ubuntu 4.8.2-19ubuntu1) 4.8.2"
+	.section	.note.GNU-stack,"",@progbits
